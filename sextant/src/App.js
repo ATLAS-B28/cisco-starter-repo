a46e8ef4a3cc15820779e0a140056978296a7fb9
ADITYA BHAMBERE <79972089+ATLAS-B28@users.noreply.github.com>
2023-06-29 10:25:25 +0000

    Added some components

--- sextant/src/App.js
+++ sextant/src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
+
 import './App.css';
 
-function App() {
+function App(props) {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className='Banner'>Sextant Banner</h1>
+      <div className='Cards'>
+        <div>Card Title</div>
+        <div className='card-1'>{props.children}</div>
+      </div>
     </div>
   );
 }
